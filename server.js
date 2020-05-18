eaf590e4a30259818e51b2c22790039ecf6576cf
iLongRanger <rortiz.dev@gmail.com>
2020-05-18 15:40:53 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+//app will start here
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API RUNNUNG');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log(`Server started on port ${PORT}`)); 
+
