de8bcaaeb5d1520ff3d16fb93bda2b83514a54c1
iLongRanger <rortiz.dev@gmail.com>
2020-05-21 22:52:43 -0700

    setup route

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,16 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
-
-
 const App = () => {
   return (
     <Router>
       <Fragment>
         <Navbar />
-        <Route exact path="/" component = {Landing} />
+        <Route exact path="/" component={Landing} />
         <section className="container">
           <Switch>
-            <Route exact path="/register" component = { Register } />
-            <Route exact path="/login" component = { Login } />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </section>
       </Fragment>
