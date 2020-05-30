7ba68be17ef6ea5a2754f6ac9c72d7ac1e872553
iLongRanger <rortiz.dev@gmail.com>
2020-05-29 23:48:06 -0700

    added Developers Link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,9 +8,14 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+
+      <li>
         <Link to="/dashboard">
-        <i className="fas fa-user"></i>{" "}
-        <span className="hide-sm">Dashboard</span></Link>
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
       </li>
       <li>
         <a onClick={logout} href="#!">
@@ -24,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="#!">Developers</Link>
+        <Link to="/profiles">Developers</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
