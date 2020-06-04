f2300da27ae8d5488107617b6a7a27b1ec33faa3
iLongRanger <rortiz.dev@gmail.com>
2020-06-04 01:09:21 -0700

    fixed get post by id route

--- routes/api/post.js
+++ routes/api/post.js
@@ -52,24 +52,23 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-// @route   GET  api/post/:id
-// @desc    Get  Post by ID
-// @access  private
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
 router.get("/:id", auth, async (req, res) => {
   try {
-    const post = await Posts.findById(req.params.id);
+    const post = await Post.findById(req.params.id);
 
-    if (!post) {
-      return res.status(404).json("Post Not Found");
+    // Check for ObjectId format and post
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/) || !post) {
+      return res.status(404).json({ msg: "Post not found" });
     }
 
     res.json(post);
   } catch (err) {
-    if (err.kind === "ObjectId") {
-      return res.status(404).json("Post Not Found");
-    }
+    console.error(err.message);
 
-    res.status(500).json("Server Error");
+    res.status(500).send("Server Error");
   }
 });
 
